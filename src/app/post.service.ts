556f6d15d5e8b521e947ca7d94b4c355073dd7c1
Ivan Backovic <ng1v4n85@gmail.com>
2023-06-22 12:39:27 +0200

    Using Subjects for Error Handling

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -2,41 +2,45 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { map } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Injectable ({providedIn: 'root'})
 export class PostsService {
+  error = new Subject<string>();
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    createAndStorePost(title: string, content: string) {
-        const postData: Post = {title: title, content: content};
-        this.http
-        .post<{ name: string }>(
-          'https://ng-complete-guide-dc2cf-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
-          postData
-        )
-        .subscribe((responseData) => {
-          console.log(responseData);
-        });
-    }
+  createAndStorePost(title: string, content: string) {
+      const postData: Post = {title: title, content: content};
+      this.http
+      .post<{ name: string }>(
+        'https://ng-complete-guide-dc2cf-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      }, error => {
+        this.error.next(error.message);
+      });
+  }
 
-    fetchPosts() {
-        return this.http
-        .get<{ [key: string]: Post }>('https://ng-complete-guide-dc2cf-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
-        .pipe(
-          map(responseData => {
-            const postsArray: Post[] = [];
-            for (const key in responseData) {
-              if (responseData.hasOwnProperty(key)) {
-                postsArray.push({ ...responseData[key], id: key });
-              }
+  fetchPosts() {
+      return this.http
+      .get<{ [key: string]: Post }>('https://ng-complete-guide-dc2cf-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+      .pipe(
+        map(responseData => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
             }
-            return postsArray;
-          })
-        )
-    }
+          }
+          return postsArray;
+        })
+      )
+  }
 
-    deletePosts() {
-        return this.http.delete('https://ng-complete-guide-dc2cf-default-rtdb.europe-west1.firebasedatabase.app/posts.json');
-    }
+  deletePosts() {
+      return this.http.delete('https://ng-complete-guide-dc2cf-default-rtdb.europe-west1.firebasedatabase.app/posts.json');
+  }
 }
