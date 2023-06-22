c7241cfd9388e3d589d748d1c80dd3439a0a2871
Ivan Backovic <ng1v4n85@gmail.com>
2023-06-22 16:46:36 +0200

    Changing the Response Body Type

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -40,7 +40,8 @@ export class PostsService {
         'https://ng-complete-guide-dc2cf-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
         {
           headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
-          params: searchParamas
+          params: searchParamas,
+          responseType: 'json'
         }
       )
       .pipe(
@@ -64,7 +65,8 @@ export class PostsService {
     return this.http.delete(
       'https://ng-complete-guide-dc2cf-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
       {
-        observe: 'events'
+        observe: 'events',
+        responseType: 'text'
       }
     ).pipe(
       tap(event => {
